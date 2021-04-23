91f2695057f2d67658a9e47cf5423863e77947f9
Sara <johndoe@example.com>
2021-04-23 09:21:46 +0200

    Added more quotes

--- src/api.js
+++ src/api.js
@@ -16,27 +16,122 @@ export const API = [
 	},
 	{
 		id: 4,
-		saying: "Hello World",
+		saying: "If You know, you know",
 		author: "Elitjon Metaliaj",
 	},
 	{
 		id: 5,
-		saying: "Hello World",
+		saying: "Sa gjykues qe jeni... S'guxon robi te beje nje gabim",
 		author: "Elitjon Metaliaj",
 	},
 	{
 		id: 6,
-		saying: "Hello World",
+		saying: "Ca ka bere kauri!",
 		author: "Elitjon Metaliaj",
 	},
 	{
 		id: 7,
-		saying: "Hello World",
+		saying: "Dalim ne negativa pastaj",
 		author: "Elitjon Metaliaj",
 	},
 	{
 		id: 8,
-		saying: "Hello World",
+		saying: "Po ca lidhje po ka?!",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 9,
+		saying: "Bej si te besh",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 10,
+		saying: "A futet kjo te BUT-i im?",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 11,
+		saying: "Eh Sara...Sara...",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 12,
+		saying: "5 here 4 26",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 13,
+		saying: "5 here 4 26",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 14,
+		saying: "Mos fol se ma hoqe mendimin",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 15,
+		saying: "Shtyu pak se e kam me mire kur shkruaj vet",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 16,
+		saying: "Po s'me pelqen shume kjo struktura...",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 17,
+		saying: "Afer mendjes",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 18,
+		saying: "Ia fut kot dhe ti...te jemi realiste",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 19,
+		saying: "As e ve ne dyshim qe e kam sakte",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 20,
+		saying: "Punet e Zotit",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 21,
+		saying: "S'ta ka fajin Zoti...",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 22,
+		saying: "Pse s'te punon kjo tastiera ty?",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 23,
+		saying: "Mos u bej compiler",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 24,
+		saying: "Aq di aq ben kjo Sara...",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 25,
+		saying: "Duhet te futesh brenda problemit",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 26,
+		saying: "Ra ne depresion",
+		author: "Elitjon Metaliaj",
+	},
+	{
+		id: 27,
+		saying: "Ra ne depresion",
 		author: "Elitjon Metaliaj",
 	},
 ];
